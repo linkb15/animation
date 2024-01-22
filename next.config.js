1bb21cbb849d10944db2ae67ef70863e3fc3daba
Borris <borris.wiria@gmail.com>
2024-01-23 03:58:46 +0400

    update next and add million

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const million = require('million/compiler');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: false,
 };
 
-module.exports = nextConfig;
+module.exports = million.next(
+  nextConfig, { auto: { rsc: true } }
+);
