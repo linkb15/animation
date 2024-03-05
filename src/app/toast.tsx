14d9ab2fb2a0de926ec31b51fa27638d4cded192
Borris <borris.wiria@gmail.com>
2024-03-05 05:48:33 +0400

    feat: SEO updated and Link to IG Reels and Reddit Post added

--- src/app/toast.tsx
+++ src/app/toast.tsx
@@ -2,6 +2,33 @@ import { toast } from "sonner";
 
 export const openInfoToast = () =>
 	toast("Welcome, thanks for the visit!", {
-		description:
-			"Open in desktop for the best view\nEnjoy the animation, and keep refreshing to check it again. I will open-source the codebase in the future. Still in WIP 🔨 atm.",
+		description: (
+			<>
+				Open in desktop for the best view.
+				<br />
+				Enjoy the animation, and keep refreshing to check it again. I will
+				open-source the codebase in the future. Still in WIP 🔨 atm.
+				<br />
+				<br />
+				Follow for more information on the{" "}
+				<a
+					className="underline text-sky-500"
+					href="https://www.reddit.com/r/nextjs/comments/18zt889/animation_is_hard/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Reddit Post
+				</a>
+				! Inspiration from this{" "}
+				<a
+					className="underline text-sky-500"
+					href="https://www.instagram.com/p/C0RdOSMLCWa/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					IG Reel
+				</a>
+				.
+			</>
+		),
 	});
