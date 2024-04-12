440011c94235f31e2b084b5332690313bd9d8d84
Borris <borris.wiria@gmail.com>
2024-04-13 01:58:12 +0800

    feat: :arrow_up: next, tailwindcss, typescript, eslint-config-next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { ReactSpring } from "@/app/react-spring";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen justify-center items-center">
+		<main className="flex min-h-screen overflow-hidden justify-center items-center">
 			<ReactSpring />
 		</main>
 	);
